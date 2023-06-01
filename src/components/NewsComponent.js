101ac26573b5bac456cbb32511d54b8a04d29a06
alfa <muhamadalfarizy20@gmail.com>
2023-06-01 19:28:53 +0700

    hook

--- src/components/NewsComponent.js
+++ src/components/NewsComponent.js
@@ -1,118 +1,102 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
-class NewsComponent extends Component {
-  constructor(props) {
-    super(props);
+const NewsComponent = () => {
+  const [newsData, setNewsData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const apiKey = '7740326d44ef443fa986f7be54b362ae';
 
-    this.state = {
-      newsData: [],
-      loading: true,
-      searchQuery: '',
-    };
+  useEffect(() => {
+    fetchData();
+  }, []); // Eksekusi fetchData hanya pada saat komponen NewsComponent pertama kali dirender
 
-    this.apiKey = '7740326d44ef443fa986f7be54b362ae';
-  }
+  useEffect(() => {
+    fetchData();
+  }, [searchQuery]); // Eksekusi fetchData ketika searchQuery berubah
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.searchQuery !== this.state.searchQuery) {
-      this.fetchData();
-    }
-  }
-
-  fetchData = () => {
-    const { searchQuery } = this.state;
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business${encodeURIComponent(
+  const fetchData = () => {
+    const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news${encodeURIComponent(
       searchQuery
-    )}&from=2023-05-01&sortBy=publishedAt&apiKey=${this.apiKey}`;
+    )}&from=2023-05-01&sortBy=publishedAt&apiKey=${apiKey}`;
 
-    this.setState({ loading: true });
+    setLoading(true);
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        this.setState({
-          loading: false,
-          newsData: data.articles || [],
-        });
+        setLoading(false);
+        setNewsData(data.articles || []);
       })
       .catch((error) => console.log(error));
   };
 
-  handleSearch = () => {
-    this.fetchData();
+  const handleSearch = () => {
+    fetchData();
   };
 
-  render() {
-    const { newsData, loading, searchQuery } = this.state;
-
-    const loadingComponent = (
-      <div className="col-12 text-center mt-5" id="loadingGun">
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+  const loadingComponent = (
+    <div className="col-12 text-center mt-5" id="loadingGun">
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
       </div>
-    );
+    </div>
+  );
 
-    const createCard = (newsData) => {
-      if (newsData && newsData.length > 0) {
-        return newsData.map((news) => (
-          <div
-            className="card col-lg-4 mx-auto my-3"
-            style={{ width: '20rem' }}
-            key={news.title}
-          >
-            <img
-              src={news.urlToImage}
-              className="card-img-top py-2"
-              alt="img"
-            />
-            <div className="card-body">
-              <h5 className="card-title">{news.title}</h5>
-              <p className="card-text">{news.description}</p>
-              <a href={news.url} className="btn btn-primary">
-                Read more
-              </a>
-            </div>
-          </div>
-        ));
-      } else {
-        return (
-          <div className="col-12 text-center mt-5 alert alert-warning">
-            Data not found
-          </div>
-        );
-      }
-    };
-
-    return (
-      <div>
-        <div className="input-group mt-5 mx-auto w-75">
-          <input
-            type="text"
-            className="form-control rounded"
-            placeholder="Search"
-            value={searchQuery}
-            onChange={(e) => this.setState({ searchQuery: e.target.value })}
+  const createCard = (newsData) => {
+    if (newsData && newsData.length > 0) {
+      return newsData.map((news) => (
+        <div
+          className="card col-lg-4 mx-auto my-3"
+          style={{ width: '20rem' }}
+          key={news.title}
+        >
+          <img
+            src={news.urlToImage}
+            className="card-img-top py-2"
+            alt="img"
           />
-          <button
-            type="button"
-            className="btn btn-primary"
-            onClick={this.handleSearch}
-          >
-            Submit
-          </button>
+          <div className="card-body">
+            <h5 className="card-title">{news.title}</h5>
+            <p className="card-text">{news.description}</p>
+            <a href={news.url} className="btn btn-primary">
+              Read more
+            </a>
+          </div>
         </div>
-        {loading ? loadingComponent : null}
-        <div className="container">
-          <div className="row">{createCard(newsData)}</div>
+      ));
+    } else {
+      return (
+        <div className="col-12 text-center mt-5 alert alert-warning">
+          Data not found
         </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <div className="input-group mt-5 mx-auto w-75">
+        <input
+          type="text"
+          className="form-control rounded"
+          placeholder="Search"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={handleSearch}
+        >
+          Submit
+        </button>
       </div>
-    );
-  }
-}
+      {loading ? loadingComponent : null}
+      <div className="container">
+        <div className="row">{createCard(newsData)}</div>
+      </div>
+    </div>
+  );
+};
 
 export default NewsComponent;
